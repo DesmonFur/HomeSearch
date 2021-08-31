fff9ba046481db6c80c0026d8c051b665acf161f
desmonFur <desfurtick@gmail.com>
2021-08-31 00:48:18 -0600

    searchbar and home list component creation with express connection

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const homeCtrl = require("./controllers/homeController");
-const { PORT } = process.env;
+const { SERVER_PORT  } = process.env;
+
 
 
 app.get("/api/home/address", homeCtrl.getByAddress);
 
-app.listen(PORT, () => console.log(`Pikachu used thunderbolt on port ${PORT}!`));
+app.listen(SERVER_PORT, () => console.log(`Pikachu used thunderbolt on port ${SERVER_PORT}!`));
