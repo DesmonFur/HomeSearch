5261165033314322429c071d420dc8b0b38514c6
desmonFur <desfurtick@gmail.com>
2021-08-30 22:06:20 -0600

    adding assets and data folders + altering getByAddress method to return list of homes including text from search query

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -3,7 +3,7 @@ const homes = require("../data/homes.json");
 module.exports = {
     getByAddress(req, res) {
         const { address } = req.query
-        const filtered = homes.filter(el => el.ADDRESS.toLowerCase() === address.toLowerCase()  )
+        const filtered = homes.filter(el => el.ADDRESS.toLowerCase().includes(address.toLowerCase()) )
         res.status(200).send( filtered ? filtered : [{}])
     }
 }
