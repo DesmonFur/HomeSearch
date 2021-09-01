c9c8f53d65d6e8ea92fecda9d7a06fb60e4da7ed
desmonFur <desfurtick@gmail.com>
2021-08-31 19:53:33 -0600

    mapping over filtered homes and returning wanted data with enhanced  keys

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -2,8 +2,19 @@ const homes = require("../data/homes.json");
 
 module.exports = {
     getByAddress(req, res) {
-        const { address } = req.query
-        const filtered = homes.filter(el => el.ADDRESS.toLowerCase().includes(address.toLowerCase()) )
-        res.status(200).send( filtered ? filtered : [{}])
+        const {address} = req.query
+        const filteredHomes = homes.filter(home => home.ADDRESS.toLowerCase().includes(address.toLowerCase()))
+        const resultArray = filteredHomes.map(home => ({
+            City: home.CITY,
+            State: home['STATE OR PROVINCE'],
+            Address: home.ADDRESS,
+            Price: new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(home.PRICE),
+            Property_type: home['PROPERTY TYPE'],
+            Bedrooms: home.BEDS,
+            Bathrooms: home.BATHS,
+            Square_Footage: home['SQUARE FEET'],
+            Year_Built: home['YEAR BUILT']
+        }));
+        res.status(200).send(resultArray)
     }
 }
