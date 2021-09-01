8995e6e0a235ca5b49f977abdc09c8c3215f9129
desmonFur <desfurtick@gmail.com>
2021-09-01 01:09:53 -0600

    adding url to resultArray

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -4,6 +4,7 @@ module.exports = {
     getByAddress(req, res) {
         const {address} = req.query
         const filteredHomes = homes.filter(home => home.ADDRESS.toLowerCase().includes(address.toLowerCase()))
+        console.log(filteredHomes)
         const resultArray = filteredHomes.map(home => ({
             City: home.CITY,
             State: home['STATE OR PROVINCE'],
@@ -13,7 +14,8 @@ module.exports = {
             Bedrooms: home.BEDS,
             Bathrooms: home.BATHS,
             Square_Footage: home['SQUARE FEET'],
-            Year_Built: home['YEAR BUILT']
+            Year_Built: home['YEAR BUILT'],
+            Home_Url:  Object.values(home[`URL (SEE http://www`].redfin)[0]
         }));
         res.status(200).send(resultArray)
     }
