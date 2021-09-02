b9c5cd9bb524eabae24088e9b5b37ebdbb7a198b
DesmonFur <desfurdev@gmail.com>
2021-09-01 22:03:19 -0600

    removed parseHomecsv script and moved the code to the homeController

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,21 +1,45 @@
-const homes = require("../data/homes.json");
+const csvToJson = require("csvtojson");
+const csvFolder = "./assets";
+const fs = require("fs");
+
+fs.readdirSync(csvFolder).forEach((file) => {
+  csvToJson()
+    .fromFile(`./assets/${file}`)
+    .then((json) => {
+      fs.writeFileSync(
+        "./server/data/homes.json",
+        JSON.stringify(json),
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    });
+});
 
 module.exports = {
-    getByAddress(req, res) {
-        const {address} = req.query
-        const filteredHomes = homes.filter(home => home.ADDRESS.toLowerCase().includes(address.toLowerCase()))
-        const resultArray = filteredHomes.map(home => ({
-            City: home.CITY,
-            State: home['STATE OR PROVINCE'],
-            Address: home.ADDRESS,
-            Price: new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(home.PRICE),
-            Property_type: home['PROPERTY TYPE'],
-            Bedrooms: home.BEDS,
-            Bathrooms: home.BATHS,
-            Square_Footage: home['SQUARE FEET'],
-            Year_Built: home['YEAR BUILT'],
-            Home_Url:  Object.values(home[`URL (SEE http://www`].redfin)[0]
-        }));
-        res.status(200).send(resultArray)
-    }
-}
+  getByAddress(req, res) {
+    const homes = require("../data/homes.json");
+    const { address } = req.query;
+    const filteredHomes = homes.filter((home) =>
+      home.ADDRESS.toLowerCase().includes(address.toLowerCase())
+    );
+    const resultArray = filteredHomes.map((home) => ({
+      City: home.CITY,
+      State: home["STATE OR PROVINCE"],
+      Address: home.ADDRESS,
+      Price: new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(home.PRICE),
+      Property_type: home["PROPERTY TYPE"],
+      Bedrooms: home.BEDS,
+      Bathrooms: home.BATHS,
+      Square_Footage: home["SQUARE FEET"],
+      Year_Built: home["YEAR BUILT"],
+      Home_Url: Object.values(home[`URL (SEE http://www`].redfin)[0],
+    }));
+    res.status(200).send(resultArray);
+  },
+};
